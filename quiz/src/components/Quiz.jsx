c253d94ce41b28c430097e4ad75b84b209e7625f
Sarthak4730 <sarthakasm07@gmail.com>
2024-06-25 22:16:21 +0530

    questions array passed as props

--- quiz/src/components/Quiz.jsx
+++ quiz/src/components/Quiz.jsx
@@ -18,18 +18,18 @@ export default function Quiz(){
         })();
     }, [])
 
-    // let questions = []
-    // for(let i=0; i<5; i++)
-    //     questions.push(data[i].question.text)
-    // console.log(questions)
+    let questions = []
+    for(let i=0; i<5; i++)
+        questions.push(data[i].question.text)
+    console.log(questions)
 
     return (
         <div className="box">
-            <Question q="How would one say goodbye in Spanish?" opts={["Adiós", "Hola", "Au Revoir", "Salir"]} />
-            <Question q="Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?" opts={["Cabbage Patch Kids", "Transformers", "Care Bears", "Rubik's Cube"]} />
-            <Question q="What is the hottest planet in our Solar System?" opts={["Mercury", "Venus", "Mars", "Saturn"]} />
-            <Question q="In which country was the caesar salad invented?" opts={["Italy", "Portugal", "Mexico", "France"]} />
-            <Question q="How Many Hearts Does An Octopus Have?" opts={["One", "Two", "Three", "Four"]} />
+            <Question q={questions[0]} opts={["Adiós", "Hola", "Au Revoir", "Salir"]} />
+            <Question q={questions[1]} opts={["Cabbage Patch Kids", "Transformers", "Care Bears", "Rubik's Cube"]} />
+            <Question q={questions[2]} opts={["Mercury", "Venus", "Mars", "Saturn"]} />
+            <Question q={questions[3]} opts={["Italy", "Portugal", "Mexico", "France"]} />
+            <Question q={questions[4]} opts={["One", "Two", "Three", "Four"]} />
             <Link to="/analysis">Check answers</Link>
         </div>
     )
