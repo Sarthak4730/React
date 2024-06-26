3bfc69b5abb7a059afecc5d760e6e0841df00f7c
Sarthak4730 <sarthakasm07@gmail.com>
2024-06-26 22:13:59 +0530

    if props.ch is undefined, .length cannot be accessed, hence used && to fix this fatal issue

--- quiz/src/components/Choice.jsx
+++ quiz/src/components/Choice.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Question(props){
-    let text = props.ch.length>60 ? props.ch.substring(0,70) + "..." : props.ch.substring(0,60)
+    let text = props.ch && ( props.ch.length>60 ? props.ch.substring(0,70) + "..." : props.ch.substring(0,60) )
     return (
         <div className="answer-option" onClick={props.onClick}>{props.ch && text }</div>
     )
