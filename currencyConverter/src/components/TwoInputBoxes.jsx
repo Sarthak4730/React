388cb15145bacaea9ab468b3fe6da98b1945d2a9
Sarthak4730 <sarthakasm07@gmail.com>
2024-06-30 11:50:35 +0530

    options rendered

--- currencyConverter/src/components/TwoInputBoxes.jsx
+++ currencyConverter/src/components/TwoInputBoxes.jsx
@@ -1,10 +1,24 @@
 export default function TwoInputBoxes(){
+    const currOptions = {
+                            "CNY": "Chinese Yuan",
+                            "CHF": "Swiss Franc",
+                            "AUD": "Australian Dollar",
+                            "PLN": "Polish Zloty",
+                            "TRY": "Turkish Lira",
+                            "GBP": "British Pound",
+                            "NZD": "New Zealand Dollar",
+                            "KRW": "South Korean Won",
+                            "DKK": "Danish Krone",
+                            "HKD": "Hong Kong Dollar"
+                        }
+    const currOptionsEntries = Object.entries(currOptions)
     return (
         <div className="two-inputs">
             <input type="number" placeholder="1"/>
             <select>
-                <option value={"USD"}> USD </option>
-                <option value={"INR"}> INR </option>
+                {currOptionsEntries.map( c => {
+                    return <option value={c[0]}> {c[1]} </option>
+                })}
             </select>
         </div>
     )
